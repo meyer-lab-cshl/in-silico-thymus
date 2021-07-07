5485d9a1c958cbefc6df58273d143f9d7202d19c
etmulle <mulle@cshl.edu>
2021-07-07 10:50:53 -0400

    Add age-based cell death and random agent generation over time

--- thymus_abm.jl
+++ thymus_abm.jl
@@ -49,7 +49,7 @@ function initialize(;
 
     space2d = ContinuousSpace(width_height, 0.02)
 
-    interactions = zeros(n_tecs, n_thymocytes) # matrix to count thymocyte/tec interactions
+    #interactions = zeros(n_tecs, n_thymocytes) # matrix to count thymocyte/tec interactions. cannot do this exactly if new agents are added over time - probably use some sort of list
 
     properties = @dict( # DrWatson @dict
         speed,
@@ -58,13 +58,14 @@ function initialize(;
         n_thymocytes,
         threshold,
         genes,
-        interactions)
+        #interactions,
+        autoreactive_proportion)
     
     model = ABM(Union{Tec, Thymocyte}, space2d; properties, rng,)
 
     # Add agents to the model
     id = 0
-    for h in 1:n_tecs
+    for _ in 1:n_tecs
         id += 1
         pos = Tuple(rand(model.rng, 2))
         vel = (0.0, 0.0)
@@ -94,7 +95,6 @@ function cell_move!(agent::Union{Tec, Thymocyte}, model)
         # add randomness to thymocytes' movements so they don't continue in same direction forever - same could be done by adding thymocyte collisions
         #walk!(agent, (rand(model.rng, -1.0:1.0),rand(model.rng, -1.0:1.0)), model) # might be unnecessary; breaks for spaces larger than (1, 1)
     end
-    agent.age += 1
 end
 
 ## Model steps
@@ -112,7 +112,7 @@ function interact!(a1::Union{Tec, Thymocyte}, a2::Union{Tec, Thymocyte}, model)
     tec_agent.num_interactions += 1
     thymocyte_agent.num_interactions += 1
 
-    model.interactions[tec_agent.id, thymocyte_agent.id - model.n_tecs] += 1 # subtract n_tecs from the thymocyte id to get thymocyte's matrix index
+    #model.interactions[tec_agent.id, thymocyte_agent.id - model.n_tecs] += 1 # subtract n_tecs from the thymocyte id to get thymocyte's matrix index
 
     if thymocyte_agent.tcr == "" # check if non-autoreactive thymocyte - is this necessary?
         return
@@ -137,19 +137,48 @@ function model_step!(model) # happens after every agent has acted
         #:types allows for easy changing of tec size by changing radius, but then thymocytes do not interact at all. :all causes error if thymocyte is deleted while having > 1 interaction. :nearest only allows for 1 interaction 
         interact!(a1, a2, model)
         elastic_collision!(a1, a2, :mass)
+
+        if a1.num_interactions % 20 == 0 # increment age after every 20 of agent's interactions
+            a1.age += 1
+        end
+        if a2.num_interactions % 20 == 0
+            a2.age += 1
+        end
     end
-    #if rand(model.rng) <= 0.02 # random chance to generate new thymocyte
-    #    model.n_thymocytes += 1
-    #    pos = Tuple(rand(model.rng, 2))
-    #    vel = sincos(2π * rand(model.rng)) .* model.speed
-    #    thymocyte = Thymocyte(nextid(model), pos, vel, 1.0, :thymocyte, "#fdbf6f", 0, 0)
-    #    add_agent!(thymocyte, model)
-    #end
+
+    if rand(model.rng) <= 0.002 # random chance to generate new thymocyte
+        #model.n_thymocytes += 1
+        pos = Tuple(rand(model.rng, 2))
+        vel = sincos(2π * rand(model.rng)) .* model.speed
+        if rand(model.rng) < model.autoreactive_proportion # set proportion of autoreactive vs non-autoreactive (empty string) thymocytes
+            tcr = rand(model.rng, model.genes)
+        else
+            tcr = ""
+        end
+        thymocyte = Thymocyte(nextid(model), pos, vel, 1.0, :thymocyte, "#fdbf6f", 10, 0, tcr, 0)
+        add_agent!(thymocyte, model)
+    end
+
+    if rand(model.rng) <= 0.002 # random chance to generate new tec
+        #model.n_tecs += 1
+        pos = Tuple(rand(model.rng, 2))
+        vel = (0.0, 0.0)
+        antis = sample(model.rng, model.genes, 2, replace = false) # choose a sample from genes to act as tec's antigens (replace = false to avoid repetitions, or are repetitions wanted?)
+        tec = Tec(nextid(model), pos, vel, Inf, :tec, "#1f78b4", 40, 0, antis, 0)
+        add_agent!(tec, model)
+    end
+
+    for agent in allagents(model) # kill agent if it reaches certain age
+        if agent.age >= 5
+            kill_agent!(agent, model)
+        end
+    end
 end
 
 model = initialize(; width_height = (1, 1), n_tecs = 5, n_thymocytes = 500, speed = 0.001, threshold = 0.8)
 cell_colors(a) = a.color
 cell_sizes(a) = a.size
+cell_markers(a) = a.type == :thymocyte ? :circle : :diamond
 
 #= abm_video(
     "thymus_abm.mp4",
@@ -182,4 +211,4 @@ parange = Dict(:threshold => 0:0.01:1)
 
 figure, adf, mdf = abm_data_exploration(
     model2, cell_move!, model_step!, parange;
-    as = cell_sizes, ac = cell_colors, adata = adata, alabels = alabels,)
+    as = cell_sizes, ac = cell_colors, am = cell_markers, adata = adata, alabels = alabels,)
